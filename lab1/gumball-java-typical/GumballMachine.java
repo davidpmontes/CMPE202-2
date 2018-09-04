efebd36c3fb500d7e21fef39262d63295e3bd68f
Tao Jinzhou <fragment.taojz@gmail.com>
2018-09-04 16:21:44 -0700

    Complete Code Maintainability

--- lab1/gumball-java-typical/GumballMachine.java
+++ lab1/gumball-java-typical/GumballMachine.java
@@ -1,43 +1,100 @@
+/**
+* The Gumball Machine class implements an application that
+* could sells gumballs to customers. 
+*
+* There are 3 types of gumball machine:
+* 1. Gumballs sells at 25 cents, only accept quarter.
+* 2. Gumballs sells at 50 cents, only accept quarter.
+* 3. Gumballs sells at 50 cents, accept kind of coins.
+* Create an specfic type of gumball machine by specificy the
+* type parameter of the constructor
+*
+* @author  Jinzhou Tao
+*/
+
+import java.util.Map;
+import java.util.HashMap;
 
 public class GumballMachine
 {
+    private int gumballNum;
+    private int gumballPrice;
+    private int insertedCoin;
+    private Map<Integer, Boolean> allowedCoin;
 
-    private int num_gumballs;
-    private boolean has_quarter;
-
-    public GumballMachine( int size )
+    public GumballMachine(int size) 
     {
-        // initialise instance variables
-        this.num_gumballs = size;
-        this.has_quarter = false;
+        this(size, 1);
     }
 
-    public void insertQuarter(int coin)
+    public GumballMachine(int size, int type)
     {
-        if ( coin == 25 )
-            this.has_quarter = true ;
+        this.gumballNum = size;
+        this.insertedCoin = 0;
+        this.allowedCoin = new HashMap<>();
+        
+        switch (type) {
+            case 3:
+                this.gumballPrice = 50;
+                this.allowedCoin.put(1, true);
+                this.allowedCoin.put(5, true);
+                this.allowedCoin.put(10, true);
+                this.allowedCoin.put(25, true);
+                this.allowedCoin.put(100, true);
+                break;
+            case 2:
+                this.gumballPrice = 50;
+                this.allowedCoin.put(1, false);
+                this.allowedCoin.put(5, false);
+                this.allowedCoin.put(10, false);
+                this.allowedCoin.put(25, true);
+                this.allowedCoin.put(100, false);
+                break;
+            case 1:
+            default:
+                this.gumballPrice = 25;
+                this.allowedCoin.put(1, false);
+                this.allowedCoin.put(5, false);
+                this.allowedCoin.put(10, false);
+                this.allowedCoin.put(25, true);
+                this.allowedCoin.put(100, false);
+                break;   
+        }
+    }
+
+    public void insertCoin(int coin)
+    {
+        if (!this.allowedCoin.containsKey(coin)){
+            System.out.println("Invalid coin number. ");
+        }
+        else if (!this.allowedCoin.get(coin))
+        {
+            System.out.println( "This coin doesn't fit this machine. Try another one." ) ;
+        } 
         else 
-            this.has_quarter = false ;
+        {
+            this.insertedCoin += coin;
+        }
     }
     
     public void turnCrank()
     {
-    	if ( this.has_quarter )
+    	if ( this.insertedCoin >= this.gumballPrice )
     	{
-    		if ( this.num_gumballs > 0 )
+    		if ( this.gumballNum > 0 )
     		{
-    			this.num_gumballs-- ;
-    			this.has_quarter = false ;
-    			System.out.println( "Thanks for your quarter.  Gumball Ejected!" ) ;
+    			this.gumballNum-- ;
+                this.insertedCoin -= this.gumballPrice;
+    			System.out.println( "Thanks for your purchase. Gumball Ejected!" ) ;
     		}
     		else
     		{
-    			System.out.println( "No More Gumballs!  Sorry, can't return your quarter." ) ;
+    			System.out.println( "No More Gumballs! Sorry, can't return your coins." ) ;
     		}
     	}
     	else 
     	{
-    		System.out.println( "Please insert a quarter" ) ;
+            System.out.println( "Please insert more coin." ) ;
     	}        
     }
 }
--- lab1/gumball-java-typical/Main.java
+++ lab1/gumball-java-typical/Main.java
@@ -3,20 +3,47 @@
 public class Main {
 
 	public static void main(String[] args) {
-		GumballMachine gumballMachine = new GumballMachine(5);
 
-		System.out.println(gumballMachine);
+        // Test type 1 Gumball Machine
+		GumballMachine gumballMachine1 = new GumballMachine(5);
+		System.out.println(gumballMachine1);
 
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
+		gumballMachine1.insertCoin( 25 );
+        gumballMachine1.turnCrank();
+        
+		gumballMachine1.insertCoin( 25 );
+		gumballMachine1.turnCrank();
+		gumballMachine1.insertCoin( 25 );
+        gumballMachine1.turnCrank();
+        
+        // Test type 2 Gumball Machine
+		GumballMachine gumballMachine2 = new GumballMachine(5, 2);
+		System.out.println(gumballMachine2);
 
-		System.out.println(gumballMachine);
+        // Test for insufficient coin
+        gumballMachine2.insertCoin( 25 );
+		gumballMachine2.turnCrank();
+		gumballMachine2.insertCoin( 25 );
+        gumballMachine2.turnCrank();
+        
+        // Test type 3 Gumball Machine
+        GumballMachine gumballMachine3 = new GumballMachine(1, 3);
+        System.out.println(gumballMachine3);
+        
+		gumballMachine3.insertCoin( 5 );
+		gumballMachine3.insertCoin( 10 );
+		gumballMachine3.insertCoin( 25 );
+		gumballMachine3.insertCoin( 10 );
+        gumballMachine3.turnCrank();
+        
+        // Test insufficent gumball
+        gumballMachine3.insertCoin( 100 );
+        gumballMachine3.turnCrank();
 
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-		gumballMachine.insertQuarter( 25 );
-		gumballMachine.turnCrank();
-
-		System.out.println(gumballMachine);
+        // Test inserting invalid coin
+		gumballMachine2.insertCoin( 2 );
+        // Test Inserting incomparable coin
+        gumballMachine2.insertCoin( 5 );
+        
 	}
 }
